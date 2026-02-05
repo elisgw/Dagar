b9f4dbeef06a7cda4ad26e7fa2926b81ac714ac3
elisgw <elisgw@kth.se>
2026-02-05 14:24:46 +0100

    Börjat med att skapa klass som ska hålla reda på allt samt skapat underfunktionerna födelse och dagens_datum.

---
+++ Dagar jag levt.py
@@ -0,0 +1,21 @@
+class dagar():
+    def __init__():
+        print ("hej")
+        
+
+
+
+    def födelse(init, år, månad, dag, ):
+        år = int(input("Vilket år föddes du? "))
+        månad = int(input("Vilken månad föddes du? "))
+        dag = int(input("Vilken dag föddes du? "))
+
+    def dagens_datum(idag):
+        idag =
+
+def __main__():
+    dagar()
+
+
+__main__()
+
